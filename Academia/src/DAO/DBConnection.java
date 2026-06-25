cc62c806f2c0faecc0c1836b36f34c21d3fcb7f2
Paulo <paulorobertosoaresdasilvasantos@aluno.itj.g12.br>
2026-06-25 09:21:54 -0300

    Update DBConnection.java

--- Academia/src/DAO/DBConnection.java
+++ Academia/src/DAO/DBConnection.java
@@ -5,46 +5,27 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-	
-	private static DBConnection instance = null;
-	private static Connection connection;
-	
-	
-	private DBConnection() {
-		
-		try {
-			if (connection == null || connection.isClosed()) {
-				connection = DriverManager.getConnection(
-						"jdbc:postgresql://localhost:5432/teste2",
-						"postgres",
-						"1234"
-						);
-				System.out.print("Conectado ao banco!");
-				}	
-		}catch (SQLException e) {
-			System.out.print("Erro ao conectar ao banco!");
-			e.printStackTrace();
-				
-			}
-		
-	}
-	
-	public Connection getConnection() {
-		return this.connection;
-	}
-	
-	public static DBConnection getInstance() {
-		
-		if (instance == null) {
-			instance = new DBConnection();
-		}
-		
-		return instance;
-	}
-	
+
+    private static DBConnection instance;
+
+    private static final String URL =
+            "jdbc:postgresql://localhost:5432/teste2";
+
+    private static final String USER = "postgres";
+
+    private static final String PASSWORD = "1234";
+
+    private DBConnection() {}
+
+    public static DBConnection getInstance() {
+        if (instance == null) {
+            instance = new DBConnection();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+        System.out.println("Abrindo conexão...");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
 }
-
-
-
-
-
